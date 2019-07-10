c8d028ed9d41c0bf41e6835ea835f8fc47028f41
dseong0224 <dseong0224@gmail.com>
2019-07-10 21:49:25 +0000

    Create a figma that includes a Header, ProductListItem, and ProductList components. Check out a branch from dev named view-products-front-end.   Define a stateful App component in client/components/app.jsx with an initial state that includes an empty Array of products.   Define a stateless Header component in client/components/header.jsx that displays the store's name and logo. Add Header to App's render method.   Define a method in App named getProducts that retrieves all products by sending a GET request to /api/products.php.   Add a componentDidMount method to App that calls getProducts.   Define a stateless ProductListItem component in client/components/product-list-item.jsx that renders a single product card.   Define a stateless ProductList component in client/components/product-list.jsx that renders a grid of ProductListItems.   Add ProductList to App's render method and pass it all of App's products as a prop can't view products on the front end yet

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,30 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react';
+import Header from './header';
+import ProductListItem from './product-list-item';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+    this.getProducts = this.getProducts.bind(this);
+  }
+  getProducts() {
+    fetch('/api/products.php')
+      .then(response => response.json())
+      .then(products => this.setState({ products }))
+      .catch(error => console.error(error));
+  }
+  componentDidMount() {
+    this.getProducts();
+  }
   render() {
-    return null;
+    return (
+      <div>
+        <Header/>
+        <ProductListItem productsFromApi={this.state.products}/>
+      </div>
+    );
+
   }
 }
---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+function ProductListItem(props) {
+
+  return (<div>
+    {/* <Card>
+          <CardBody>
+            <CardTitle>{product[0].name}</CardTitle>
+            <CardSubtitle>{product[0].price}</CardSubtitle>
+          </CardBody>
+          <CardBody>
+            <CardText>{product[0].shortDescription}</CardText>
+            <CardImg href={product[0].image}>Another Link</CardImg>
+          </CardBody>
+        </Card> */}
+    {/* {console.log(props.productsFromApi[0])} */}
+  </div>
+  );
+
+}
+
+export default ProductListItem;
