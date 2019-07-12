872715e38764a842cf2f2bac1f8d36dd1d7c81db
dseong0224 <dseong0224@gmail.com>
2019-07-12 20:26:01 +0000

    completed place-an-order

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -3,18 +3,18 @@ import CartSummaryItem from './cart-summary-item';
 
 export default function CartSummary(props) {
   function listCartItems() {
-    return (props.cartStateProps.map(cartItem => <CartSummaryItem key={cartItem.id} data={cartItem} viewCartItemDetails={props.updateViewState}/>));
+    if (props.cartStateProps.length === 0) {
+      return <h1>CART IS EMPTY...</h1>;
+    }
+    return (props.cartStateProps.map(cartItem => {
+      return <CartSummaryItem key={cartItem.id} data={cartItem} viewCartItemDetails={props.updateViewState}/>;
+    }));
   }
   function resetSetView() {
     props.updateViewState('catalog', {});
   }
-  function totalPrice() {
-    const itemPriceArray = props.cartStateProps.map(cartItem => cartItem.price);
-    let priceTotal = 0;
-    for (var priceIndex = 0; priceIndex < itemPriceArray.length; priceIndex++) {
-      priceTotal += itemPriceArray[priceIndex];
-    }
-    return '$' + (priceTotal / 100).toFixed(2);
+  function viewCheckoutForm() {
+    props.updateViewState('checkout', {});
   }
   if (props.nameStateProps === 'cart') {
     return (
@@ -23,7 +23,7 @@ export default function CartSummary(props) {
           <div>
             <div className="btn-group btn-group-toggle" data-toggle="buttons">
               <label className="btn btn-secondary active">
-                <a onClick={resetSetView} checked>Back to Catalog</a>
+                <button onClick={resetSetView} checked>Back to Catalog</button>
               </label>
             </div>
             <div>
@@ -34,7 +34,8 @@ export default function CartSummary(props) {
           <div className="container">
             <div className="col">
               {listCartItems()}
-              Total: {totalPrice()}
+                        Total: {totalPrice(props.cartStateProps)}
+              <button onClick={viewCheckoutForm}>Checkout</button>
             </div>
           </div>
         </div>
@@ -48,4 +49,13 @@ export default function CartSummary(props) {
       </div>
     </div>
   );
+}
+
+function totalPrice(cartItems) {
+  const itemPriceArray = cartItems.map(cartItem => cartItem.price);
+  let priceTotal = 0;
+  for (var priceIndex = 0; priceIndex < itemPriceArray.length; priceIndex++) {
+    priceTotal += itemPriceArray[priceIndex];
+  }
+  return '$' + (priceTotal / 100).toFixed(2);
 }
