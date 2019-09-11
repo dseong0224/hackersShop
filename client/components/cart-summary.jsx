738a8d84a99ebf52ffc65fa0924a361161a39c6d
dseong0224 <dseong0224@gmail.com>
2019-09-11 02:44:08 +0000

    console working

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,75 +1,87 @@
 import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
-export default function CartSummary(props) {
-  function listCartItems() {
-    if (props.cart.length === 0) {
+export default class CartSummary extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  listCartItems() {
+    if (this.props.cart.length === 0) {
       return <h1>CART IS EMPTY...</h1>;
     }
-    return (props.cart.map(cartItem => {
-      return <CartSummaryItem key={cartItem.id} data={cartItem} viewCartItemDetails={props.setPage} getCartItems={props.getCartItems} remove={props.remove} updateCart={props.updateCart}/>;
+    return (this.props.cart.map(cartItem => {
+      return <CartSummaryItem key={cartItem.id} data={cartItem} viewCartItemDetails={this.props.setPage} getCartItems={this.props.getCartItems} remove={this.props.remove} updateCart={this.props.updateCart}/>;
     }));
   }
-  function resetSetView() {
-    props.setPage('catalog', {});
+  resetSetView() {
+    this.props.setPage('catalog', {});
   }
-  function viewCheckoutForm() {
-    props.setPage('checkout', {});
+  viewCheckoutForm() {
+    this.props.setPage('checkout', {});
   }
-  if (props.page === 'cart') {
-    return (
-      <div className="mt-4 mb-4 container">
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="h3">CART <span className="h6 text-muted">{props.cartQuantity} item(s)</span></div>
-            <div className="container">
-              <div className="mt-3 mb-3">
-                <hr/>
-                {listCartItems()}
+
+  calculateSubTotal() {
+    // console.log('hello');
+  }
+
+  render() {
+    if (this.props.page === 'cart') {
+      return (
+        <div className="mt-4 mb-4 container">
+          <div className="row">
+            <div className="col-sm-7">
+              <div className="h3">CART <span className="h6 text-muted">{this.props.cartQuantity} item(s)</span></div>
+              <div className="container">
+                <div className="mt-3 mb-3">
+                  <hr/>
+                  {this.listCartItems()}
+                </div>
               </div>
             </div>
+            <div className="col-sm-5">
+              <h3> Summary </h3><hr/>
+              <div className="h6">Subtotal: <span className="float-right">$10,000.00{this.calculateSubTotal()}</span></div>
+              <div className="h6"> Shipping: <span className="float-right">FREE</span></div>
+              <div className="h6 mb-4">Tax: <span className="float-right">$1,500.00</span></div>
+              <hr/>
+              <div className="h5 mb-4">TOTAL : <span className="float-right">$11,500.00</span></div>
+              <button type="button" className="btn btn-light border border-success btn-block" onClick={this.resetSetView} style={{ cursor: 'pointer' }}>BACK TO SHOPPING</button>
+              <button type="button" className="btn btn-light border border-success btn-block" onClick={this.viewCheckoutForm} style={{ cursor: 'pointer' }}>CHECKOUT</button>
+            </div>
           </div>
-          <div className="col-sm-5">
-            <h3> Summary </h3><hr/>
-            <div className="h6">Subtotal: <span className="float-right">{subTotalPrice(props.cart)}</span></div>
-            <div className="h6"> Shipping: <span className="float-right">FREE</span></div>
-            <div className="h6 mb-4">Tax: <span className="float-right">{totalTax(props.cart)}</span></div>
-            <hr/>
-            <div className="h5 mb-4">TOTAL : <span className="float-right">{totalPrice(props.cart)}</span></div>
-            <button type="button" className="btn btn-light border border-success btn-block" onClick={resetSetView} style={{ cursor: 'pointer' }}>BACK TO SHOPPING</button>
-            <button type="button" className="btn btn-light border border-success btn-block" onClick={viewCheckoutForm} style={{ cursor: 'pointer' }}>CHECKOUT</button>
-          </div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div>
+          Page Loading...
         </div>
       </div>
     );
   }
-  return (
-    <div>
-      <div>
-          Page Loading...
-      </div>
-    </div>
-  );
+
 }
 
-function subTotalPrice(cartItems) {
-  let priceTotal = 0;
-  for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
-    priceTotal += parseFloat(cartItems[priceIndex].price);
-  }
-  return '$' + (priceTotal / 100).toFixed(2);
-}
-function totalTax(cartItems) {
-  let priceTotal = 0;
-  for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
-    priceTotal += parseFloat(cartItems[priceIndex].price);
-  }
-  return '$' + (priceTotal * 0.9 / 1000).toFixed(2);
-}
-function totalPrice(cartItems) {
-  let priceTotal = 0;
-  for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
-    priceTotal += parseFloat(cartItems[priceIndex].price);
-  }
-  return '$' + (priceTotal * 1.09 / 100).toFixed(2);
-}
+// function subTotalPrice(cartItems) {
+//   let priceTotal = 0;
+//   for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
+//     priceTotal += parseFloat(cartItems[priceIndex].price);
+//   }
+//   return '$' + (priceTotal / 100).toFixed(2);
+// }
+// function totalTax(cartItems) {
+//   let priceTotal = 0;
+//   for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
+//     priceTotal += parseFloat(cartItems[priceIndex].price);
+//   }
+//   return '$' + (priceTotal * 0.9 / 1000).toFixed(2);
+// }
+// function totalPrice(cartItems) {
+//   let priceTotal = 0;
+//   for (var priceIndex = 0; priceIndex < cartItems.length; priceIndex++) {
+//     priceTotal += parseFloat(cartItems[priceIndex].price);
+//   }
+//   return '$' + (priceTotal * 1.09 / 100).toFixed(2);
+// }
