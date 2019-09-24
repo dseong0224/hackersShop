9caa455c26a41952bf450acecceb655f2bba0da2
dseong0224 <dseong0224@gmail.com>
2019-09-24 18:32:59 +0000

    added final confirmation modal at the end of order placement

--- client/components/checkout-form-confirmation-modal.jsx
+++ client/components/checkout-form-confirmation-modal.jsx
@@ -11,12 +11,12 @@ export default class CheckoutConfirmationModal extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <button type="button" className="btn btn-success btn-block" data-toggle="modal" data-target="#exampleModalLong" onClick={this.props.placeOrder}>PLACE ORDER</button>
+        <button type="button" className="btn btn-success btn-block mt-2 mb-2" data-toggle="modal" data-target="#exampleModalLong" onClick={this.props.placeOrder}>PLACE ORDER</button>
         <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="card-font modal-header">
-                <h5 className="modal-title"><i className="fas fa-exclamation-circle text-warning"></i> FINAL CONFIRMATION</h5>
+                <h5 className="modal-title">ORDER CONFIRMATION</h5>
                 <button type="button" className="close" aria-label="Close" data-dismiss="modal">
                   <span aria-hidden="true">×</span></button>
               </div>
@@ -24,26 +24,15 @@ export default class CheckoutConfirmationModal extends React.Component {
                 <div className="container mb-3 text-center description-font font-weight-bold">Would you like to submit your order?</div>
                 <CheckoutModalDetail/>
                 <hr/>
-                <div className="h3 card-font text-center font-weight-bold">Order Total: $5068.86</div>
-                <div className="description-font text-muted text-center">15 item(s)</div>
+                <div className="h3 card-font text-center font-weight-bold">Order Total: ${this.props.orderTotal}</div>
+                <div className="description-font text-muted text-center">{this.props.cartQuantity} item(s)</div>
                 <div className="align-content-center">
                   <CheckoutModalItem/>
-                  <div className="mt-3 mb-3"><hr/>
-                    <div className="row align-items-center mt-1 mb-1">
-                      <img src="./media/2-1.png" alt="" className="col-sm-5 mx-auto"/>
-                      <div className="col-sm-7">
-                        <div className="h6 card-font">Zoom Lebron III</div>
-                        <div className="h6 card-font text-muted">Houston</div>
-                        <div className="h6 description-font text-muted">$175.00</div>
-                        <div className="h6">Quantity: 1</div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
               <div className="card-font modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">RETURN TO CHECKOUT</button>
-                <button type="button" className="btn btn-success">SUBMIT ORDER</button></div>
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">CANCEL</button>
+                <button type="button" className="btn btn-success">SUBMIT</button></div>
             </div>
           </div>
         </div>
--- client/components/checkout-summary.jsx
+++ client/components/checkout-summary.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 export default function ListCartItems(props) {
+
+  // function calculateSubTotal() {
+  //   let cart = props.cart;
+  //   let subTotal = 0;
+  //   for (let cartItemIndex = 0; cartItemIndex < cart.length; cartItemIndex++) {
+  //     subTotal += cart[cartItemIndex].count * cart[cartItemIndex].price;
+  //   }
+  //   return (subTotal / 100).toFixed(2);
+  // }
+
   return (
+
     <li className="list-group-item d-flex justify-content-between lh-condensed" >
       <div>
         <h6 className="my-0">{props.cartItem.name}</h6>
@@ -11,3 +22,25 @@ export default function ListCartItems(props) {
     </li>
   );
 }
+
+// { /* <div class="mt-3 mb-3"><hr/>
+//                     <div class="row align-items-center mt-1 mb-1"><img src="./media/3-1.png" alt="" class="col-sm-5 mx-auto"/>
+//                     <div class="col-sm-7">
+//                       <div class="h6 card-font">Air Jordan I</div>
+//                       <div class="h6 card-font text-muted">Sail/Gym Red</div>
+//                       <div class="h6 description-font text-muted">$160.00</div>
+//                       <div class="h6">Quantity: 2</div>
+//                       </div>
+//                       </div>
+//                       </div>
+//                       <div class="mt-3 mb-3"><hr/>
+//                       <div class="row align-items-center mt-1 mb-1">
+//                         <img src="./media/7-1.png" alt="" class="col-sm-5 mx-auto"/>
+//                         <div class="col-sm-7">
+//                           <div class="h6 card-font">Adapt BB</div>
+//                           <div class="h6 card-font text-muted">Wolf Grey</div>
+//                           <div class="h6 description-font text-muted">$350.00</div>
+//                           <div class="h6">Quantity: 12</div>
+//                           </div>
+//                           </div>
+//                           </div> */ }
