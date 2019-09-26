6d3744c91251dea74793e2a6ddb96aa2141bd0ac
dseong0224 <dseong0224@gmail.com>
2019-09-26 23:38:33 +0000

    at the end of order, redirects to landing page and clears out cart

--- client/components/payment-info-validation.jsx
+++ client/components/payment-info-validation.jsx
@@ -11,8 +11,10 @@ export default class PaymentInfoValidation extends React.Component {
     this.setState({ creditCardNumber: event.target.value });
     if (event.target.value.length !== 16) {
       this.validation = false;
+      this.textColor = 'text-success';
     } else {
       this.validation = true;
+      this.textColor = 'text-danger';
     }
   }
 
@@ -22,9 +24,10 @@ export default class PaymentInfoValidation extends React.Component {
         <div className="card-body">
           <p className="card-text"> Credit Card </p>
           <div className="mb-1 input-group"><input id="cc-number" placeholder="1234567891011121" name="creditCard" type="number" className="form-control" onChange={this.handleChange}/></div>
+          <p className="text-success">Valid Credit Card Number</p>
           <div className="mb-1 input-group"><input placeholder="Exp. Date" type="number" className="form-control"/></div>
           <div className="mb-1 input-group"><input placeholder="CVV" type="number" className="form-control"/></div>
-          <p className="text-success">Valid Credit Card Number</p>
+
         </div>
       );
     }
@@ -32,9 +35,9 @@ export default class PaymentInfoValidation extends React.Component {
       <div className="card-body">
         <p className="card-text"> Credit Card </p>
         <div className="mb-1 input-group"><input id="cc-number" placeholder="1234567891011121" name="creditCard" type="number" className="form-control" onChange={this.handleChange}/></div>
-        <div className="mb-1 input-group"><input placeholder="Exp. Date" type="number" className="form-control"/></div>
+        <p className="text-danger">Credit Card Number must be 16 digits</p>
+        <div className="mb-1 input-group"><input placeholder="Exp. Date" type="number" maxLength="4" className="form-control"/></div>
         <div className="mb-1 input-group"><input placeholder="CVV" type="number" className="form-control"/></div>
-        <p className="text-danger">credit card number must be 16 digits</p>
 
       </div>);
   }
