bf4a83f546bf6ca34662534d6dd55ee1a87b1b22
dseong0224 <dseong0224@gmail.com>
2019-09-27 06:22:58 +0000

    added previous conditional code

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -32,28 +32,28 @@ export default class ProductListItem extends React.Component {
       justifyContent: 'center',
       alignItems: 'center'
     };
-    // if (this.state.currentProduct.hasOwnProperty('id')) {
-    //   return (
-    //     <div className="mt-2 mb-2 d-flex align-items-stretch col-12 col-sm-6 col-md-4 col-lg-3">
-    //       <div className="card-font text-center shadow-sm card">
-    //         <h6 className="card-title text-black bg-light" style={{ display: 'inline', flexDirection: 'row', padding: 10, height: '75px' }}>{this.props.product.name}</h6>
-    //         <div style={imageStyle}>
-    //           <img onClick = {this.viewProduct} src={this.props.product.image} style={{ width: '70%', cursor: 'pointer' }} alt={this.props.product.name}/>
-    //         </div>
-    //         <div className="card-body">
-    //           <div className="h6 mb-3">${(this.props.product.price / 100).toFixed(2)}</div>
-    //           <button type="button" className="mr-1 ml-1 mb-2 btn  btn-outline-secondary" style={{ cursor: 'pointer' }} onClick = {this.viewProduct}>View Detail</button>
-    //           <ProductListItemConfirmationModal
-    //             addToCart = {this.addToCart}
-    //             setPage={this.props.setPage}
-    //             name={this.state.currentProduct.name}
-    //             image={this.state.currentProduct.image}
-    //             price={this.state.currentProduct.price}/>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   );
-    // }
+    if (this.state.currentProduct.hasOwnProperty('id')) {
+      return (
+        <div className="mt-2 mb-2 d-flex align-items-stretch col-12 col-sm-6 col-md-4 col-lg-3">
+          <div className="card-font text-center shadow-sm card">
+            <h6 className="card-title text-black bg-light" style={{ display: 'inline', flexDirection: 'row', padding: 10, height: '75px' }}>{this.props.product.name}</h6>
+            <div style={imageStyle}>
+              <img onClick = {this.viewProduct} src={this.props.product.image} style={{ width: '70%', cursor: 'pointer' }} alt={this.props.product.name}/>
+            </div>
+            <div className="card-body">
+              <div className="h6 mb-3">${(this.props.product.price / 100).toFixed(2)}</div>
+              <button type="button" className="mr-1 ml-1 mb-2 btn  btn-outline-secondary" style={{ cursor: 'pointer' }} onClick = {this.viewProduct}>View Detail</button>
+              <ProductListItemConfirmationModal
+                addToCart = {this.addToCart}
+                setPage={this.props.setPage}
+                name={this.state.currentProduct.name}
+                image={this.state.currentProduct.image}
+                price={this.state.currentProduct.price}/>
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="mt-2 mb-2 d-flex align-items-stretch col-12 col-sm-6 col-md-4 col-lg-3">
         <div className="card-font text-center shadow-sm card">
